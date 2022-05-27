f853e9cafaf3bfb3b37d4f1de526ea1f048abb6b
Archer921o <lunev_alex70@mail.ru>
2022-05-27 11:04:26 +0300

    Result app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,54 @@
-var main = function(toDoObjects) {
-    "use strict";
-    var toDos = toDoObjects.map(function(toDo) {
-        // просто возвращаем описание
-        // этой задачи
-        return toDo.description;
-    });
-    // сейчас весь старый код должен работать в точности как раньше!
-    // ...
-};
-$(document).ready(function() {
-    $.getJSON("todos.json", function(toDoObjects) {
-        // вызываем функцию main с задачами в качестве аргумента
-        main(toDoObjects);
-    });
+"use strict";
+
+var toDos = [
+    "Закончить писать эту книгу",
+    "Вывести Греи?си на прогулку в парк",
+    "Ответить на электронные письма",
+    "Подготовиться к лекции в понедельник",
+    "Обновить несколько новых задач",
+    "Купить продукты",
+];
+
+$("document").ready(function() {
+    $(".tabs a span")
+        .toArray()
+        .forEach(function(element) {
+            // создаем обработчик щелчков для этого элемента
+            $(element).on("click", function() {
+                // поскольку мы используем версию элемента jQuery,
+                // нужно создать временную переменную,
+                // чтобы избежать постоянного обновления
+                var $element = $(element);
+                $(".tabs a span").removeClass("active");
+                $(element).addClass("active");
+                $("main .content").empty();
+
+                if ($element.parent().is(":nth-child(1)")) {
+                    for (var i = toDos.length - 1; i > -1; i--) {
+                        $(".content").append($("<li>").text(toDos[i]));
+                    }
+                } else if ($element.parent().is(":nth-child(2)")) {
+                    toDos.forEach(function(todo) {
+                        $(".content").append($("<li>").text(todo));
+                    });
+                } else if ($element.parent().is(":nth-child(3)")) {
+                    $(".content").append(
+                        '<input type="text" class="inp">' +
+                        '<button class="btn">Добавить</button>'
+                    );
+                    var newToDo;
+                    $(".btn").on("click", function() {
+                        newToDo = $(".inp").val();
+                        if (newToDo != "") {
+                            toDos.push(newToDo);
+                            alert('Новое задание "' + newToDo + '" успешно добавлено!');
+                            $(".inp").val("");
+                        }
+                    });
+                }
+                return false;
+            });
+        });
+
+    $(".tabs a:first-child span").trigger("click");
 });
-$(document).ready(main);
